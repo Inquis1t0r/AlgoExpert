585ef1d75ab701849c458142b840b0ac94af2e65
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-06-03 15:02:27 +0200

    Create ValidateSubsequence.cs

---
+++ C#/ValidateSubsequence.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+public class Program {
+	public static bool IsValidSubsequence(List<int> array, List<int> sequence) {
+		// Write your code here.
+		return false;
+	}
+}
