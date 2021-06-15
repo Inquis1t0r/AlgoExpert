00a9f5f6872ca46dd2310f5f98c0eddad2c84c6a
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-06-15 16:47:34 +0200

    Create BranchSums.cs

---
+++ C#/BranchSums.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+public class Program {
+	// This is the class of the input root. Do not edit it.
+	public class BinaryTree {
+		public int value;
+		public BinaryTree left;
+		public BinaryTree right;
+
+		public BinaryTree(int value) {
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+	public static List<int> BranchSums(BinaryTree root) {
+		// Write your code here.
+		return new List<int>();
+	}
+}
