791f10a3fb6abdc4c57070312f990cbb22f78d3e
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-06-15 16:47:49 +0200

    Create NodeDepths.cs

---
+++ C#/NodeDepths.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Program {
+	public static int NodeDepths(BinaryTree root) {
+		// Write your code here.
+		return -1;
+	}
+
+	public class BinaryTree {
+		public int value;
+		public BinaryTree left;
+		public BinaryTree right;
+
+		public BinaryTree(int value) {
+			this.value = value;
+			left = null;
+			right = null;
+		}
+	}
+}
