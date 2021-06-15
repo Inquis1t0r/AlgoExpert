804352247699a0e12843d2b5ed1082c6c20499eb
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-06-15 16:47:15 +0200

    Create FindClosestValueInBST.cs

---
+++ C#/FindClosestValueInBST.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Program {
+	public static int FindClosestValueInBst(BST tree, int target) {
+		// Write your code here.
+		return -1;
+	}
+
+	public class BST {
+		public int value;
+		public BST left;
+		public BST right;
+
+		public BST(int value) {
+			this.value = value;
+		}
+	}
+}
