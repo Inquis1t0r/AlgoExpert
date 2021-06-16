6de89428e49222f208eadc61015eefcc543ee57b
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-06-16 16:11:22 +0200

    Create FindClosestValueInBST.java

---
+++ Java/FindClosestValueInBST.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Program {
+  public static int findClosestValueInBst(BST tree, int target) {
+    // Write your code here.
+    return -1;
+  }
+
+  static class BST {
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
+}
