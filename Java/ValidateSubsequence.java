28dc9a6737f16d0509ed5257e1c9ce871ca5ee59
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-06-16 16:09:05 +0200

    Create ValidateSubsequence.java

---
+++ Java/ValidateSubsequence.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Program {
+  public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+    // Write your code here.
+    return false;
+  }
+}
