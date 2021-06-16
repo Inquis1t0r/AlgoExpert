45d2fdfe3477a73b0f93a9ac46ec46680572f140
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-06-16 16:10:55 +0200

    Create TournamentWinner.java

---
+++ Java/TournamentWinner.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Program {
+
+  public String tournamentWinner(
+      ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
+    // Write your code here.
+    return "";
+  }
+}
