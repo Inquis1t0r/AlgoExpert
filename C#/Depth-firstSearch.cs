e4f37b8a4f718f47e14a9563b6c16a3ba998ff6f
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-06-21 15:14:00 +0200

    Update Depth-firstSearch.cs

--- C#/Depth-firstSearch.cs
+++ C#/Depth-firstSearch.cs
@@ -1,27 +1,28 @@
-import java.util.*;
+using System;
+using System.Collections.Generic;
 
-class Program {
-  // Do not edit the class below except
-  // for the depthFirstSearch method.
-  // Feel free to add new properties
-  // and methods to the class.
-  static class Node {
-    String name;
-    List<Node> children = new ArrayList<Node>();
+public class Program {
+	// Do not edit the class below except
+	// for the DepthFirstSearch method.
+	// Feel free to add new properties
+	// and methods to the class.
+	public class Node {
+		public string name;
+		public List<Node> children = new List<Node>();
 
-    public Node(String name) {
-      this.name = name;
-    }
+		public Node(string name) {
+			this.name = name;
+		}
 
-    public List<String> depthFirstSearch(List<String> array) {
-      // Write your code here.
-      return array;
-    }
+		public List<string> DepthFirstSearch(List<string> array) {
+			// Write your code here.
+			return array;
+		}
 
-    public Node addChild(String name) {
-      Node child = new Node(name);
-      children.add(child);
-      return this;
-    }
-  }
+		public Node AddChild(string name) {
+			Node child = new Node(name);
+			children.Add(child);
+			return this;
+		}
+	}
 }
