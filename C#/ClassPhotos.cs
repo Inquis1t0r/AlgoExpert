85c7ab4a145f3902ce9bf854b97be3f418ce979e
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-06-21 15:12:56 +0200

    Create ClassPhotos.cs

---
+++ C#/ClassPhotos.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+
+
+public class Program {
+
+	public bool ClassPhotos(List<int> redShirtHeights, List<int> blueShirtHeights) {
+		// Write your code here.
+		return false;
+	}
+}
+
