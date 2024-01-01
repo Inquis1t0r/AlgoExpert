1c2020ef7c5d0ac8325ca6e1708ba2d6e783f440
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-01-01 12:11:33 +0100

    Update TwoNumberSum.java

--- Java/TwoNumberSum.java
+++ Java/TwoNumberSum.java
@@ -2,7 +2,20 @@ import java.util.*;
 
 class Program {
   public static int[] twoNumberSum(int[] array, int targetSum) {
+
+    List<int> resultList = new ArrayList<int>();
     // Write your code here.
-    return new int[0];
+    for(int i = 0; i<array.length;i++){
+      for(int j = array.length; j>0;j--){
+        if(i + j == targetSum){
+          System.out.println(targetSum +" "+i + " " + j);
+          resultList.add(i);
+          resultList.add(j);
+        }
+      }
+    }
+
+    int[] result = resultList.toArray(new int[0]);
+    return result;
   }
 }
