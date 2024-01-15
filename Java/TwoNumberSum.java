188919c736233239a39cf1ee37c45f511b42aafd
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-01-15 09:46:19 +0100

    Update TwoNumberSum.java

--- Java/TwoNumberSum.java
+++ Java/TwoNumberSum.java
@@ -7,9 +7,12 @@ class Program {
             for (int i = 0; i < array.length - 1; i++) {
               for (int j = i + 1; j < array.length; j++) {
                   if (array[i] + array[j] == targetSum) {
-                      result[0] = i;
-                      result[1] = j;
+                      result[0] = array[i];
+                      result[1] = array[j];
                       return result;
+                  }else{
+                    result = new int[]{};
+                    return result;
                   }
               }
         }
