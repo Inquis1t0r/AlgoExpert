19c41ecf925af3196f49e8f28469b77b71006d9c
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-01-15 09:32:15 +0100

    Update two-number-sum.js

--- JavaScript/two-number-sum.js
+++ JavaScript/two-number-sum.js
@@ -3,7 +3,8 @@ function twoNumberSum(array, targetSum) {
       for (let i = 0; i < array.length - 1; i++) {
         for (let j = i + 1; j < array.length; j++) {
             if (array[i] + array[j] === targetSum) {
-                return [i, j];
+                // If a pair is found, return their indices
+                return [array[i], array[j]];
             }
         }
     }
