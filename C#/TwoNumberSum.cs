6e0431b2f354cb83db7d5e9dc5ae21916d6e4293
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-01-15 10:07:07 +0100

    Update TwoNumberSum.cs

--- C#/TwoNumberSum.cs
+++ C#/TwoNumberSum.cs
@@ -1,8 +1,18 @@
 using System;
 
 public class Program {
-	public static int[] TwoNumberSum(int[] array, int targetSum) {
-		// Write your code here.
-		return new int[0];
+  public static int[] TwoNumberSum(int[] array, int targetSum) {
+      int[] result = new int[2];
+    // Write your code here.
+	    for (int i = 0; i < array.Length - 1; i++) {
+	      for (int j = i + 1; j < array.Length; j++) {
+		  if (array[i] + array[j] == targetSum) {
+		      result[0] = array[i];
+		      result[1] = array[j];
+		      return result;
+		  	}
+		}
 	}
+    return new int[0];
+  }
 }
