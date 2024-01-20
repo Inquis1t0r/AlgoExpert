12f0e2382f8efb27047f113eddea8c5f887bf8dc
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-01-20 11:45:37 +0100

    Create palindrome-check.cs

---
+++ C#/palindrome-check.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program {
+  public static bool IsPalindrome(string str) {
+    // Write your code here.
+      str = str.ToLower();
+
+      int left = 0;
+      int right = str.Length -1;
+
+      
+    return false;
+  }
+}
