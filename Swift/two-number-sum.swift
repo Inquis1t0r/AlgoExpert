86cdb285dbb12011e6342132520b607e78b30c15
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-01-28 16:15:39 +0100

    Update two-number-sum.swift

--- Swift/two-number-sum.swift
+++ Swift/two-number-sum.swift
@@ -1,7 +1,17 @@
 class Program {
   func twoNumberSum(_ array: inout [Int], _ targetSum: Int) -> [Int] {
     // Write your code here.
+        var result = [Int]()
+
+    for i in 0..<array.count - 1 {
+        for j in i + 1..<array.count {
+            if array[i] + array[j] == targetSum {
+                result.append(array[i])
+                result.append(array[j])
+                return result
+            }
+        }
+    }
     return []
   }
 }
-
