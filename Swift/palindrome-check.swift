78a91dfbf550de33398a4cabe86b57d2d97c3f8a
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-01-29 09:22:40 +0100

    Update palindrome-check.swift

--- Swift/palindrome-check.swift
+++ Swift/palindrome-check.swift
@@ -1,6 +1,18 @@
 class Program {
   func isPalindrome(string: String) -> Bool {
     // Write your code here.
-    return false
+    let length = string.count
+    let middleIndex = length / 2
+
+      for i in 0..<middleIndex {
+        let frontIndex = string.index(string.startIndex, offsetBy: i)
+        let backIndex = string.index(string.endIndex, offsetBy: -i - 1)
+        
+        if string[frontIndex] != string[backIndex] {
+            return false
+        }
+  }
+    
+    return true
   }
 }
