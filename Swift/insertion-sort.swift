a17eb33c877d527483ae2c1b2d55db6aa898e0f6
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-02-09 12:59:15 +0100

    Update insertion-sort.swift

--- Swift/insertion-sort.swift
+++ Swift/insertion-sort.swift
@@ -1,6 +1,15 @@
 class Program {
   func insertionSort(array: inout [Int]) -> [Int] {
-    // Write your code here.
-    return []
+  var sortedArray = array
+  for i in 1..<sortedArray.count {
+    var key = sortedArray[i]
+    var j = i - 1
+    while j >= 0 && sortedArray[j] > key {
+      sortedArray[j + 1] = sortedArray[j]
+      j -= 1
+    }
+    sortedArray[j + 1] = key
+  }
+  return sortedArray
   }
 }
