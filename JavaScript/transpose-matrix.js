a776d25df0fbebef1ec821fa3ac93770374fa10a
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-03-08 15:41:37 +0100

    Update transpose-matrix.js

--- JavaScript/transpose-matrix.js
+++ JavaScript/transpose-matrix.js
@@ -1,6 +1,20 @@
 function transposeMatrix(matrix) {
   // Write your code here.
-  return [];
+  const switchedArr = [];
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+    for (let i = 0; i < cols; i++) {
+      switchedArr.push(new Array(rows).fill(null));
+    }
+  
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+          switchedArr[j][i] = matrix[i][j];
+      }
+    }
+  
+  return switchedArr;
 }
 
 // Do not edit the line below.
