b8d3eb0366391b5d6499096263dac8322f94dc12
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-04-18 18:06:50 +0200

    Update validate-subsequence.js

--- JavaScript/validate-subsequence.js
+++ JavaScript/validate-subsequence.js
@@ -1,5 +1,18 @@
 function isValidSubsequence(array, sequence) {
   // Write your code here.
+  let seqIndex = 0;
+  let subPointer = sequence[seqIndex];
+
+
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] == subPointer){
+      seqIndex++;
+      console.log('match: ', array[i], subPointer);
+    }
+  }
+  console.log(seqIndex);
+  console.log(sequence.length);
+return seqIndex == sequence.length;
 }
 
 // Do not edit the line below.
