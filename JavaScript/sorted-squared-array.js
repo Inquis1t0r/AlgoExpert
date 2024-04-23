db23acc734ec1dfd7ae988659f2544b85ce6be9d
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-04-23 13:19:33 +0200

    Update sorted-squared-array.js

--- JavaScript/sorted-squared-array.js
+++ JavaScript/sorted-squared-array.js
@@ -1,6 +1,8 @@
 function sortedSquaredArray(array) {
   // Write your code here.
-  return [];
+  return array.map(function (x) {
+    return Math.pow(x, 2);
+  }).sort((a, b) => a - b);
 }
 
 // Do not edit the line below.
