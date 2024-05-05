f9c4a1d99f46d4adf99e696626f13a082d235143
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-05-05 15:54:30 +0200

    Create middle-node.js

---
+++ JavaScript/middle-node.js
@@ -0,0 +1,17 @@
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+exports.LinkedList = LinkedList;
+
+function middleNode(linkedList) {
+  // Write your code here.
+  return null;
+}
+
+// Do not edit the line below.
+exports.middleNode = middleNode;
