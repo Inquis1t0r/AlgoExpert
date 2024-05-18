3fc1061c14e0aeed49725280922278b5e72b82f9
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-05-18 19:46:00 +0200

    Create first-non-repeating-character.js

---
+++ JavaScript/first-non-repeating-character.js
@@ -0,0 +1,7 @@
+function firstNonRepeatingCharacter(string) {
+  // Write your code here.
+  return -1;
+}
+
+// Do not edit the line below.
+exports.firstNonRepeatingCharacter = firstNonRepeatingCharacter;
