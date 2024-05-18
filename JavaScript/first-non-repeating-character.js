ce5073b305633b33e08a70d7c30f073d331b42d0
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-05-18 19:58:39 +0200

    Update first-non-repeating-character.js

--- JavaScript/first-non-repeating-character.js
+++ JavaScript/first-non-repeating-character.js
@@ -1,5 +1,11 @@
 function firstNonRepeatingCharacter(string) {
   // Write your code here.
+  for (let letter of string) {
+    let isDuplicated = string.indexOf(letter) !== string.lastIndexOf(letter);
+      if (!(isDuplicated)) {
+        return string.indexOf(letter);
+      }
+  }
   return -1;
 }
 
