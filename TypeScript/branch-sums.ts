45c36c23b0d0cbd6de4c7255a1023328b4f33c7d
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-05-26 21:08:24 +0200

    Create branch-sums.ts

---
+++ TypeScript/branch-sums.ts
@@ -0,0 +1,18 @@
+// This is the class of the input root.
+// Do not edit it.
+class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function branchSums(root: BinaryTree): number[] {
+  // Write your code here.
+  return [-1];
+}
