132f1ebe3ac722a7533c2ef8aeeab94291aeac23
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-06-07 21:39:20 +0200

    Update common-characters.js

--- JavaScript/common-characters.js
+++ JavaScript/common-characters.js
@@ -1,8 +1,13 @@
 function commonCharacters(strings) {
   // Write your code here.
-  //map<char, count>
-  const charMap = new Map();
-  return [];
+    let commonChars = new Set(strings[0]);
+  
+    for (let i = 1; i < strings.length; i++) {
+        let currentChars = new Set(strings[i]);
+        commonChars = new Set([...commonChars].filter(char => currentChars.has(char)));
+    }
+
+    return [...commonChars];
 }
 
 // Do not edit the line below.
