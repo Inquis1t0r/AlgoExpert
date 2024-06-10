0f099449a3f079baa040582e7c1a1eb7459aed87
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-06-10 18:24:40 +0200

    Update binary-search.js

--- JavaScript/binary-search.js
+++ JavaScript/binary-search.js
@@ -1,5 +1,25 @@
 function binarySearch(array, target) {
-  // Write your code here.
+    // Write your code here.
+    let low = array[0];
+    let high = array[array.length - 1];
+    let result = -1;
+    while (low <= high) {
+        let mid = (low + high) / 2;
+
+        if (array[mid] == target) {
+            result = mid;
+        }
+
+        if (array[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+
+
+    }
+
+    return result;
 }
 
 // Do not edit the line below.
