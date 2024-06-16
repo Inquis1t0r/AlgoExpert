ec57b36d0bcf0ae6d53feff0838278ebf4c02703
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-06-16 18:21:51 +0200

    Update two-number-sum.cpp

--- C++/two-number-sum.cpp
+++ C++/two-number-sum.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 vector<int> twoNumberSum(vector<int> array, int targetSum) {
   // Write your code here.
-  return {};
+
+  vector<int> resultList;
+  for(int i = 0; i<array.size(); i++){
+    for(int j = 0; j < array.size(); j++){
+      int localSum = array[i] + array[j];
+      if(targetSum == localSum && i != j){
+        resultList.push_back(array[i]);
+      }
+    }
+  }
+  
+  return resultList;
 }
