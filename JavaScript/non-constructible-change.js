bc7bb9c5cb4f95a17f92ab18af42e073fd675f78
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-06-22 19:10:41 +0200

    Update non-constructible-change.js

--- JavaScript/non-constructible-change.js
+++ JavaScript/non-constructible-change.js
@@ -1,6 +1,18 @@
 function nonConstructibleChange(coins) {
   // Write your code here.
-  return 1;
+
+  coins.sort((a, b) => a - b);
+
+  let res = 1;
+
+  for (let i = 0; i < coins.length; i++) {
+    if (coins[i] > res) {
+      break;
+    }
+    res += coins[i];
+  }
+
+  return res;
 }
 
 // Do not edit the line below.
